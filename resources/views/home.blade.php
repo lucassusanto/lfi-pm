bd97df1c672331ab71cb19fb1e61aafcc129aad2
Lucas <lucassusanto@gmail.com>
2018-11-25 15:13:53 +0700

    Update auth process

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,11 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'LFI - Home')
+
+@section('content_header')
+    <h1>Welcome!</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+    <p>Welcome to this admin panel. You are  <b>Admin</b>.</p>
+@stop
